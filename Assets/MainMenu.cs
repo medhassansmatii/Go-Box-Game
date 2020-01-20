2c5fa229bf4ecd6f2c01c394368d8055b31a3dd2
Hassansmati <medhassansmati@gmail.com>
2020-01-20 15:10:54 +0100

    first commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	public void playGame(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void QuitGame(){
+		Debug.Log ("Quit !");
+		Application.Quit();
+	}
+	public void setVolume (float Volume){
+	
+		audioMixer.SetFloat ("Volume", Volume);
+	
+	}
+}
